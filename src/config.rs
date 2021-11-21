ed294c258087faae36592d44025dcc8480ffb697
Joe Burnard <spongybacon@hotmail.co.uk>
2021-11-21 00:56:54 +0000

    proto support

--- src/config.rs
+++ src/config.rs
@@ -1,37 +1,50 @@
-use crate::result::Result;
 use crate::error::Error;
-use os_type::OSType;
-use std::fmt::Formatter;
+use crate::result::Result;
 use serde::{Deserialize, Serialize};
+use std::fmt::Formatter;
 use tracing::*;
 
-const CONFIGURATION_NAME: &str = "Config";
+const CONFIGURATION_NAME: &str = "fdb-proto-cli";
+const VERSION: &str = "0.1.0";
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct FdbCliConfig {
     // fdb-cli version
-    version: u8,
+    version: String,
 
     // path to cluster file
     pub cluster_file: String,
+
+    // path to the protobuf file
+    pub proto_file: Option<String>,
 }
 
 pub fn load_config() -> Result<FdbCliConfig> {
-    let config = match confy::load::<FdbCliConfig>(&CONFIGURATION_NAME) {
+    let config = match confy::load::<FdbCliConfig>(CONFIGURATION_NAME) {
         Ok(res) => {
-            info!("Found fdb-cli configuration file (version: {:?})", res.version);
+            info!(
+                "Found fdb-cli configuration file (version: {:?})",
+                res.version
+            );
             res
-        },
-        Err(e) => return Err(
-            Error::UnableToReadConfig(e)
-        )
+        }
+        Err(e) => return Err(Error::UnableToReadConfig(e)),
     };
     Ok(config)
 }
 
 impl std::fmt::Display for FdbCliConfig {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Configuration values: \nversion: {:?} \ncluster file: {}", self.version, &self.cluster_file)
+        write!(
+            f,
+            "Configuration values:
+version: {:?}
+cluster file: {}
+protobuf file: {}",
+            self.version,
+            &self.cluster_file,
+            &self.proto_file.clone().unwrap_or_default()
+        )
     }
 }
 
@@ -40,8 +53,9 @@ impl std::default::Default for FdbCliConfig {
         let path = FdbCliConfig::default_cluster_file();
 
         Self {
-            version: 0,
-            cluster_file: String::from(path)
+            version: VERSION.to_string(),
+            cluster_file: String::from(path),
+            proto_file: None,
         }
     }
 }
@@ -50,23 +64,14 @@ impl FdbCliConfig {
     pub fn default_cluster_file() -> &'static str {
         let os_type = os_type::current_platform().os_type;
         match os_type {
-            // OS Path
-            os_type::OSType::OSX => {
-                "/usr/local/etc/foundationdb/fdb.cluster"
-            }
+            // OSX Path
+            os_type::OSType::OSX => "/usr/local/etc/foundationdb/fdb.cluster",
             // All other types are unix based systems
-            _ => {
-                "/etc/foundationdb/fdb.cluster"
-            }
+            _ => "/etc/foundationdb/fdb.cluster",
         }
     }
 
     pub fn write(&self) -> Result<()> {
-        match confy::store(&CONFIGURATION_NAME, self) {
-            Ok(()) => Ok(()),
-            Err(e) => return Err(
-                Error::UnableToWriteConfig(e)
-            )
-        }
+        confy::store(CONFIGURATION_NAME, self).map_err(Error::UnableToWriteConfig)
     }
 }
--- src/error.rs
+++ src/error.rs
@@ -1,23 +1,33 @@
-use std::fmt::Formatter;
 use foundationdb::FdbError;
+use std::fmt::Formatter;
 
 #[derive(Debug)]
 pub enum Error {
     UnsupportedOperatingSystem(&'static str),
     UnableToReadConfig(std::io::Error),
     UnableToWriteConfig(std::io::Error),
+    UnableToReadProtobuf(std::io::Error),
     Fdb(FdbError),
-    Elapsed(tokio::time::error::Elapsed)
+    Elapsed(tokio::time::error::Elapsed),
+    ProtofishParseError(protofish::context::ParseError),
+    StringDecodeError(std::string::FromUtf8Error),
 }
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match *self {
             Error::UnsupportedOperatingSystem(os) => write!(f, "Unsupported OS: {}", os),
-            Error::UnableToReadConfig(ref err) => write!(f, "Unable to read configuration: {}", err),
-            Error::UnableToWriteConfig(ref err) => write!(f, "Unable to write configuration: {}", err),
+            Error::UnableToReadConfig(ref err) => {
+                write!(f, "Unable to read configuration: {}", err)
+            }
+            Error::UnableToWriteConfig(ref err) => {
+                write!(f, "Unable to write configuration: {}", err)
+            }
+            Error::UnableToReadProtobuf(ref err) => write!(f, "Unable to read protobuf: {}", err),
             Error::Fdb(ref e) => write!(f, "Fdb error: {}", e),
-            Error::Elapsed(ref e) => write!(f, "Tokio timeout elapsed error: {}", e)
+            Error::Elapsed(ref e) => write!(f, "Tokio timeout elapsed error: {}", e),
+            Error::ProtofishParseError(ref e) => write!(f, "Protofish parse error: {}", e),
+            Error::StringDecodeError(ref e) => write!(f, "String decode error: {}", e),
         }
     }
 }
@@ -37,5 +47,11 @@ impl From<FdbError> for Error {
 impl From<tokio::time::error::Elapsed> for Error {
     fn from(err: tokio::time::error::Elapsed) -> Error {
         Error::Elapsed(err)
+    }
+}
+
+impl From<protofish::context::ParseError> for Error {
+    fn from(err: protofish::context::ParseError) -> Error {
+        Error::ProtofishParseError(err)
     }
 }
